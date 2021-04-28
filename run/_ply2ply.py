13d49f56458a2577a26fc571cb2a78dc5130c03e
jrugis <j.rugis@auckland.ac.nz>
2021-04-28 16:38:45 +1200

    rearrange custom ply contents

--- run/_ply2ply.py
+++ run/_ply2ply.py
@@ -93,6 +93,25 @@ pfile = open(OUT, "w")
 pfile.write("ply\n")
 pfile.write("format ascii 1.0\n")
 pfile.write("comment Mini-Gland mesh format 1.0\n")
+pfile.write("element duct_node " + str(ndnodes) + "\n")
+pfile.write("property float x\n")
+pfile.write("property float y\n")
+pfile.write("property float z\n")
+pfile.write("element duct_segment " + str(ndsegs) + "\n")
+pfile.write("property int node_in\n")
+pfile.write("property int node_out\n")
+pfile.write("property float inner_diameter\n")
+pfile.write("property float outer_diameter\n")
+pfile.write("property int seg_type\n")
+pfile.write("property int ncells\n")
+pfile.write("property int icells\n")
+pfile.write("element cell " + str(ncells) + "\n")
+pfile.write("property int nverts\n")
+pfile.write("property int iverts\n")
+pfile.write("property int nfaces\n")
+pfile.write("property int ifaces\n")
+pfile.write("property int ntetrahedron\n")
+pfile.write("property int itetrahedron\n")
 pfile.write("element vertex " + str(tnverts) + "\n")
 pfile.write("property float x\n")
 pfile.write("property float y\n")
@@ -107,21 +126,34 @@ pfile.write("property int v1\n")
 pfile.write("property int v2\n")
 pfile.write("property int v3\n")
 pfile.write("property int v4\n")
-pfile.write("element cell " + str(ncells) + "\n")
-pfile.write("property list int int face_index\n")
-pfile.write("property list int int tetrahedron_index\n")
-pfile.write("element duct_node " + str(ndnodes) + "\n")
-pfile.write("property float x\n")
-pfile.write("property float y\n")
-pfile.write("property float z\n")
-pfile.write("element duct_segment " + str(ndsegs) + "\n")
-pfile.write("property int vertex_in\n")
-pfile.write("property int vertex_out\n")
-pfile.write("property float inner_diameter\n")
-pfile.write("property float outer_diameter\n")
-pfile.write("property int duct_type\n")
-pfile.write("property list int int cell_index\n")
 pfile.write("end_header\n")
+
+# --- write out the duct node data
+for n in dnodes:
+  pfile.write("{:.2f} {:.2f} {:.2f}\n".format(*n))
+
+# --- write out the duct segment data
+for i, s in enumerate(dsegs):
+  # -- segment details
+  pfile.write("{:d} {:d} {:.2f} {:.2f} {:d} ".format(s.node_in, s.node_out, s.inner_diameter, s.outer_diameter, s.duct_type))
+  # -- cell indices for this segment
+  icells = np.where(np.array(cell_dsegs) == i)
+  pfile.write(str(len(icells[0])) + " ")
+  pfile.write(str(icells[0][0]) + "\n")
+
+# --- write out the cell data
+pi_offset = 0  # point index offset
+fi_offset = 0  #  face   "     " 
+for fname in flist:
+  pfile.write(str(mesh.n_points) + " ");
+  pfile.write(str(pi_offset) + " ");
+  pi_offset += mesh.n_points;
+  # -- number faces in this cell
+  pfile.write(str(mesh.n_faces) + " ");
+  pfile.write(str(fi_offset) + " ");
+  fi_offset += mesh.n_faces;
+  # -- number of tetrahedrons in this cell
+  pfile.write("0 0\n");
 
 # --- write out the vertex data (all cells)
 for fname in flist:
@@ -141,34 +173,8 @@ for i, fname in enumerate(flist):
     pfile.write(str(get_ftype(mesh.points[pi], d.inner_diameter, d.outer_diameter)) + "\n")
   pi_offset += mesh.n_points
 
-# ---write out oriented tetrahedron data (all cells)
+# --- write out oriented tetrahedron data (all cells)
 # NOTE: no tets for now
-
-# --- write out the cell data
-fi_offset = 0  # face index offset
-for fname in flist:
-  # --- output the number faces in this cell
-  pfile.write(str(mesh.n_faces) + " ")
-  # -- output the face indices for this cell
-  for i in range(mesh.n_faces):
-    pfile.write(str(i + fi_offset) + " ") # the face indices increment over all the cells
-  # --- output the number of tetrahedrons in this cell
-  pfile.write("0")
-  pfile.write("\n")
-  fi_offset += mesh.n_faces 
-
-# --- write out the duct node data
-for n in dnodes:
-  pfile.write("{:.2f} {:.2f} {:.2f}\n".format(*n))
-
-# --- write out the duct segment data
-for i, s in enumerate(dsegs):
-  pfile.write("{:d} {:d} {:.2f} {:.2f} {:d} ".format(s.node_in, s.node_out, s.inner_diameter, s.outer_diameter, s.duct_type))
-  icells = np.where(np.array(cell_dsegs) == i)
-  pfile.write(str(len(icells[0])))
-  for ic in icells[0]:
-    pfile.write(" " + str(ic)) # the cell indices for this segment
-  pfile.write("\n")
 
 # --- done
 pfile.close()
